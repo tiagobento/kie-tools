17edddea47a42438c50462b196c55c063ef2ee2d
Tiago <tiagobentofernandes@gmail.com>
2023-01-25 10:10:59 -0300

    DMN Runner table outputs working

--- packages/online-editor/src/editor/DmnRunner/DmnRunnerDrawerPanelContent.tsx
+++ packages/online-editor/src/editor/DmnRunner/DmnRunnerDrawerPanelContent.tsx
@@ -288,7 +288,7 @@ export function DmnRunnerDrawerPanelContent(props: Props) {
         return newData;
       });
     },
-    [dmnRunnerState.currentInputRowIndex, dmnRunnerDispatch.setInputRows]
+    [dmnRunnerDispatch, dmnRunnerState.currentInputRowIndex]
   );
 
   const [selectedRow, selectRow] = useState<string>("");
@@ -312,7 +312,7 @@ export function DmnRunnerDrawerPanelContent(props: Props) {
           Row {rowIndex + 1}
         </DropdownItem>
       )),
-    [dmnRunnerState.inputRows]
+    [dmnRunnerDispatch, dmnRunnerState.inputRows]
   );
 
   const formInputs = useMemo(() => {
@@ -326,7 +326,7 @@ export function DmnRunnerDrawerPanelContent(props: Props) {
       selectRow(`Row ${newData.length}`);
       return newData;
     });
-  }, [dmnRunnerDispatch.setInputRows, dmnRunnerDispatch]);
+  }, [dmnRunnerDispatch]);
 
   const onChangeToTableView = useCallback(() => {
     dmnRunnerDispatch.setMode(DmnRunnerMode.TABLE);
