f08a65993dc90f182e9ef84200a4edfd1d3cf692
Tiago <tiagobentofernandes@gmail.com>
2023-01-25 22:53:55 -0300

    Improve DMN Runner usage of BeeTable

--- packages/online-editor/src/devDeployments/DevDeploymentsDropdown.tsx
+++ packages/online-editor/src/devDeployments/DevDeploymentsDropdown.tsx
@@ -164,16 +164,14 @@ export function DevDeploymentsDropdown() {
         return [
           deployments.data
             .sort((a, b) => b.creationTimestamp.getTime() - a.creationTimestamp.getTime())
-            .map((deployment, i) => {
-              return (
-                <DevDeploymentsDropdownItem
-                  key={deployment.creationTimestamp.getTime()}
-                  id={i}
-                  deployment={deployment}
-                  cloudAuthSession={authSession}
-                />
-              );
-            }),
+            .map((deployment, i) => (
+              <DevDeploymentsDropdownItem
+                key={deployment.creationTimestamp.getTime()}
+                id={i}
+                deployment={deployment}
+                cloudAuthSession={authSession}
+              />
+            )),
           <Divider key={"delete-all-separator"} inset={{ default: "insetMd" }} />,
           <DropdownItem
             key={"delete-all-deployments-dropdown-button"}
@@ -193,14 +191,12 @@ export function DevDeploymentsDropdown() {
       }
     } else {
       return [
-        <>
-          <EmptyState>
-            <EmptyStateIcon icon={PficonSatelliteIcon} />
-            <Title headingLevel="h4" size="md" style={{ color: "darkgray" }}>
-              {`Choose a Cloud provider to see your Dev deployments.`}
-            </Title>
-          </EmptyState>
-        </>,
+        <EmptyState key={"empty-state"}>
+          <EmptyStateIcon icon={PficonSatelliteIcon} />
+          <Title headingLevel="h4" size="md" style={{ color: "darkgray" }}>
+            {`Choose a Cloud provider to see your Dev deployments.`}
+          </Title>
+        </EmptyState>,
       ];
     }
   }, [authSession, deleteAllDeployments, deployments.data, deployments.status, i18n]);
