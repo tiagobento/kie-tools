75e43e340bb15f608189a824c695a04a91eb014f
Yeser Amer <yeser.amer@ibm.com>
2023-02-27 13:23:20 +0100

    Headers fixed

--- packages/boxed-expression-component/src/expressions/ExpressionDefinitionHeaderMenu/ExpressionDefinitionHeaderMenu.tsx
+++ packages/boxed-expression-component/src/expressions/ExpressionDefinitionHeaderMenu/ExpressionDefinitionHeaderMenu.tsx
@@ -76,19 +76,19 @@ export function ExpressionDefinitionHeaderMenu({
   const popoverMenuRef = useRef<PopoverMenuRef>();
 
   useEffect(() => {
-    setExpressionName(selectedExpressionName);
+    setExpressionName((prevState) => (prevState !== selectedExpressionName ? selectedExpressionName : prevState));
   }, [selectedExpressionName]);
 
   useEffect(() => {
-    setDataType(selectedDataType);
+    setDataType((prevState) => (prevState !== selectedDataType ? selectedDataType : prevState));
   }, [selectedDataType]);
 
   const onExpressionNameChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setExpressionName(event.target.value);
+    setExpressionName((prevState) => (prevState !== event.target.value ? event.target.value : prevState));
   }, []);
 
   const onDataTypeChange = useCallback((dataType: DmnBuiltInDataType) => {
-    setDataType(dataType);
+    setDataType((prevState) => (prevState !== dataType ? dataType : prevState));
   }, []);
 
   const openDataTypePage = useCallback(() => {
@@ -96,12 +96,14 @@ export function ExpressionDefinitionHeaderMenu({
   }, [beeGwtService]);
 
   const saveExpression = useCallback(() => {
-    onExpressionHeaderUpdated({ name: expressionName, dataType: dataType });
+    if (expressionName !== selectedExpressionName || dataType !== selectedDataType) {
+      onExpressionHeaderUpdated({ name: expressionName, dataType: dataType });
+    }
   }, [expressionName, onExpressionHeaderUpdated, dataType]);
 
   const resetFormData = useCallback(() => {
-    setExpressionName(selectedExpressionName);
-    setDataType(selectedDataType);
+    setExpressionName((prevState) => (prevState !== selectedExpressionName ? selectedExpressionName : prevState));
+    setDataType((prevState) => (prevState !== selectedDataType ? selectedDataType : prevState));
   }, [selectedExpressionName, selectedDataType]);
 
   const onHide = useCallback(() => {
