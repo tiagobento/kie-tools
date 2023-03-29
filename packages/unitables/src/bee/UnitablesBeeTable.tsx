1ce32bb6e575d9d344b797d853b8da084f6dd992
Luiz Motta <luizjoaomotta@gmail.com>
2023-03-29 12:40:40 -0300

    WIP keyboard shortcuts

--- packages/unitables/src/bee/UnitablesBeeTable.tsx
+++ packages/unitables/src/bee/UnitablesBeeTable.tsx
@@ -25,11 +25,13 @@ import {
 import { BoxedExpressionEditorI18n } from "@kie-tools/boxed-expression-component/dist/i18n";
 import { StandaloneBeeTable } from "@kie-tools/boxed-expression-component/dist/table/BeeTable/StandaloneBeeTable";
 import {
+  SelectionPart,
   useBeeTableCoordinates,
   useBeeTableSelectableCellRef,
+  useBeeTableSelectionDispatch,
 } from "@kie-tools/boxed-expression-component/dist/selection/BeeTableSelectionContext";
 import * as React from "react";
-import { useCallback, useMemo, useReducer, useEffect, useRef } from "react";
+import { useCallback, useMemo, useReducer, useEffect, useRef, useState } from "react";
 import * as ReactTable from "react-table";
 import { UnitablesColumnType, UnitablesInputsConfigs, UnitablesCellConfigs } from "../UnitablesTypes";
 import "@kie-tools/boxed-expression-component/dist/@types/react-table";
@@ -39,6 +41,7 @@ import { useField } from "uniforms";
 import { AUTO_ROW_ID } from "../uniforms/UnitablesJsonSchemaBridge";
 import getObjectValueByPath from "lodash/get";
 import { useUnitablesContext } from "../UnitablesContext";
+import { getOperatingSystem, OperatingSystem } from "@kie-tools-core/operating-system";
 
 export const UNITABLES_COLUMN_MIN_WIDTH = 150;
 
@@ -98,15 +101,27 @@ export function UnitablesBeeTable({
       if (column.insideProperties) {
         for (const insideProperty of column.insideProperties) {
           acc[getColumnAccessor(insideProperty)] = (props) => (
-            <UnitablesBeeTableCell {...props} joinedName={insideProperty.joinedName} />
+            <UnitablesBeeTableCell
+              {...props}
+              joinedName={insideProperty.joinedName}
+              rowCount={rows.length}
+              columnCount={columns.length}
+            />
           );
         }
       } else {
-        acc[getColumnAccessor(column)] = (props) => <UnitablesBeeTableCell {...props} joinedName={column.joinedName} />;
+        acc[getColumnAccessor(column)] = (props) => (
+          <UnitablesBeeTableCell
+            {...props}
+            joinedName={column.joinedName}
+            rowCount={rows.length}
+            columnCount={columns.length}
+          />
+        );
       }
       return acc;
     }, {} as NonNullable<BeeTableProps<ROWTYPE>["cellComponentByColumnAccessor"]>);
-  }, [columns]);
+  }, [columns, rows.length]);
 
   const setColumnWidth = useCallback(
     (fieldName: string) => (newWidthAction: React.SetStateAction<number | undefined>) => {
@@ -197,11 +212,11 @@ function getColumnAccessor(c: UnitablesColumnType) {
   return `field-${c.joinedName}`;
 }
 
-function replacer(_: any, value: string) {
-  return value.replace(/[^\w\s]/gi, "");
-}
-
-function UnitablesBeeTableCell({ joinedName }: BeeTableCellProps<ROWTYPE> & { joinedName: string }) {
+function UnitablesBeeTableCell({
+  joinedName,
+  rowCount,
+  columnCount,
+}: BeeTableCellProps<ROWTYPE> & { joinedName: string; rowCount: number; columnCount: number }) {
   const { containerCellCoordinates } = useBeeTableCoordinates();
   const { internalChange } = useUnitablesContext();
   const [{ field, value, onChange }] = useField(joinedName, {});
@@ -231,29 +246,10 @@ function UnitablesBeeTableCell({ joinedName }: BeeTableCellProps<ROWTYPE> & { jo
         onChange(newValueWithoutSymbols === "true");
       } else if (field.type === "array") {
         console.log("ARRAY", newValue);
-        // TODO: Luiz - paste can be afected by this. check ListField;
-        try {
-          const parsedValue = JSON.parse(newValue);
-          if (Array.isArray(parsedValue)) {
-            onChange(parsedValue);
-          } else {
-            onChange([]);
-          }
-        } catch (err) {
-          onChange([]);
-        }
+        // FIXME: Luiz - array fields are still not supported;
       } else if (field.type === "object" && typeof newValue !== "object") {
         console.log("OBJECT", newValue);
-        try {
-          const parsedValue = JSON.parse(newValue);
-          if (parsedValue && typeof parsedValue === "object" && !Array.isArray(parsedValue)) {
-            onChange(parsedValue);
-          } else {
-            onChange({});
-          }
-        } catch (err) {
-          onChange({});
-        }
+        // objects are flattened in a single row - this case shouldn't happen;
       } else {
         onChange(newValue);
       }
@@ -268,60 +264,73 @@ function UnitablesBeeTableCell({ joinedName }: BeeTableCellProps<ROWTYPE> & { jo
     useCallback(() => `${value ?? ""}`, [value])
   );
 
+  const { mutateSelection } = useBeeTableSelectionDispatch();
+  const navigateVertically = useCallback(
+    (args: { isShiftPressed: boolean }) => {
+      mutateSelection({
+        part: SelectionPart.ActiveCell,
+        columnCount: () => columnCount,
+        rowCount,
+        deltaColumns: 0,
+        deltaRows: args.isShiftPressed ? -1 : 1,
+        isEditingActiveCell: false,
+        keepInsideSelection: true,
+      });
+    },
+    [mutateSelection, columnCount, rowCount]
+  );
+
   // This useEffect forces the focus into the selected cell;
   useEffect(() => {
     if (isActive) {
-      const listener = (e: KeyboardEvent) => {
-        const input = cellRef.current?.getElementsByTagName("input")?.[0] as HTMLInputElement | undefined;
+      const input = cellRef.current?.getElementsByTagName("input")?.[0] as HTMLInputElement | undefined;
 
-        if (e.type === "keyup") {
-          if (e.code.toLowerCase() === "control" || e.code.toLowerCase() === "meta") {
-            return;
-          }
-          if (e.code.toLowerCase() === "enter") {
-            input?.focus();
-            return;
-          }
-          return;
-        }
+      const keyDownListener = (e: KeyboardEvent) => {
+        const complementaryKey =
+          (getOperatingSystem() === OperatingSystem.MACOS && e.metaKey) ||
+          (getOperatingSystem() !== OperatingSystem.MACOS && e.ctrlKey);
 
         if (
-          (e.ctrlKey || e.metaKey) &&
-          (e.code.toLowerCase() === "c" ||
-            e.code.toLowerCase() === "v" ||
-            e.code.toLowerCase() === "x" ||
-            e.code.toLowerCase() === "a")
+          complementaryKey &&
+          (e.key.toLowerCase() === "c" ||
+            e.key.toLowerCase() === "v" ||
+            e.key.toLowerCase() === "x" ||
+            e.key.toLowerCase() === "a")
         ) {
           return;
         }
 
-        if (e.code.toLowerCase() === "tab") {
+        if (complementaryKey || e.altKey || e.key.toLowerCase() === "shift" || e.key.toLowerCase() === "capslock") {
+          return;
+        }
+
+        if (e.key.toLowerCase() === "tab") {
           return;
         }
 
         input?.select();
       };
 
+      const keyUpListener = (e: KeyboardEvent) => {
+        if (e.key.toLowerCase() === "enter") {
+          input?.focus();
+          return;
+        }
+        return;
+      };
+
       // keyup handles "enter" code
-      document?.addEventListener("keyup", listener);
-      document?.addEventListener("keydown", listener);
+      document?.addEventListener("keyup", keyUpListener);
+      document?.addEventListener("keydown", keyDownListener);
       return () => {
-        document?.removeEventListener("keyup", listener);
-        document?.removeEventListener("keydown", listener);
+        document?.removeEventListener("keyup", keyUpListener);
+        document?.removeEventListener("keydown", keyDownListener);
       };
     }
-  }, [isActive, isEditing]);
+  }, [isActive]);
 
-  const onKeyDown = useCallback(
+  const onKeyUp = useCallback(
     (e: React.KeyboardEvent<HTMLDivElement>) => {
-      if (isEditing && e.code.toLowerCase() === "tab") {
-        return;
-      }
-
-      if (isEditing && e.code.toLowerCase() === "enter") {
-        return;
-      }
-
       if (isEditing) {
         e.stopPropagation();
       }
@@ -329,17 +338,26 @@ function UnitablesBeeTableCell({ joinedName }: BeeTableCellProps<ROWTYPE> & { jo
     [isEditing]
   );
 
-  const onKeyUp = useCallback(
+  const onKeyDown = useCallback(
     (e: React.KeyboardEvent<HTMLDivElement>) => {
-      if (isEditing && e.code.toLowerCase() === "enter") {
+      if (isEditing && e.key.toLowerCase() === "tab") {
         return;
+      }
+
+      if (isEditing && e.key.toLowerCase() === "enter") {
+        navigateVertically({ isShiftPressed: e.shiftKey });
+        return;
+      }
+
+      if (e.key.toLowerCase() === "escape") {
+        // onChange(`${cellValue}`)
       }
 
       if (isEditing) {
         e.stopPropagation();
       }
     },
-    [isEditing]
+    [isEditing, navigateVertically]
   );
 
   return (
